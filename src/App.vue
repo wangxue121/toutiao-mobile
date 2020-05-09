787a749b6a0d5599af2abc0b1786dd378131750e
wangxue <2394512956@qq.com>
2020-05-09 17:13:13 +0800

    字体图标

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
-    <router-view/>
   </div>
 </template>
 
 <script>
+import './style/index.less'
 export default {
   name: '',
   components: {},
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载Vant组件库
+import Vant from 'vant'
+// 加载Vant 组件库样式
+import 'vant/lib/index.css'
+
+// 加载 自动设置 REM 基准值（html 标签字体大小）
+import 'amfe-flexible'
+
+// 全局注册 Vant 中的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
