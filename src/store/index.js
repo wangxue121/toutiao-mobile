5f6b370f096cab93698fdd0eca2a6c06e80d89c2
wangxue <2394512956@qq.com>
2020-05-12 01:36:35 +0800

    登录-token

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
+
+// 这样做的目的可以避免访问和获取数据的名字不一致导致的问题
+const USER_KEY = 'toutiao-user'
+
 // 把数据放到容器里面的好处是，获取方便，数据发生变化时视图可以更新
 // 为了数据可以持久化，把数据放到本地存储中
 export default new Vuex.Store({
   // state 用来共享数据的就好比是组件中的data
   state: {
     // user: null, // 当前用户的登录状态(token等数据)
-    user: JSON.parse(window.localStorage.getItem('user')
-    )
+    // user: JSON.parse(window.localStorage.getItem('user'))
+    user: getItem(USER_KEY)
   },
   // 修改容器中的state要用mutations
   // data是传入的数据
@@ -18,7 +23,8 @@ export default new Vuex.Store({
       state.user = data
 
       // 为了防止页面刷新后数据丢失，所以要把数据存储到本地存储（就是为了数据持久化）
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      // window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem(USER_KEY, state.user)
     }
   },
   actions: {
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,1 +1,26 @@
 // 本地存储封装模块
+
+export const getItem = name => {
+  const data = window.localStorage.getItem(name)
+  // 把JSON.parse 放到 try-catch 中是因为data可能不是JSON格式的字符串
+  try {
+    // 把data转为JavaScript对象
+    // 如果转换成功 那么他就是JSON格式的字符串
+    return JSON.parse(data)
+    // 如果报错那么就直接原样返回
+  } catch (err) {
+    // data 不是JSON格式的字符串 直接原样返回
+    return data
+  }
+}
+export const setItem = (name, value) => {
+  // 如果value 是对象，就把value 转为JSON格式的字符串再存储
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  // 否则直接原样返回
+  window.localStorage.setItem(name, value)
+}
+export const removeItem = name => {
+  window.localStorage.removeItem(name)
+}
